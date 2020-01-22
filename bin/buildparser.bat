f4deb492d5f8487c2e862adb5eb6ce0cabf5d556
Jonathan Revusky <revusky@gmail.com>
2020-01-22 19:22:55 +0100

    Okay, I think I have it working on cygwin etcetera.

--- bin/buildparser.bat
+++ bin/buildparser.bat
@@ -1,8 +1,8 @@
 @echo off
 
-REM If there is a generated javacc.jar in the base directory, use that, otherwise, use
+REM Build the JavaCC grammar
 REM the bootstrap javacc.jar that is in the bootstrap directory.
 
-java -classpath "%~f0\..\..\build;%~f0\..\..\bootstrap\javacc.jar;%~f0\..\..\bootstrap\freemarker.jar" javacc.Main %~f0\..\..\src\grammars\JavaCC.javacc
+java -jar ""%~f0\..\javacc.jar" %~f0\..\..\src\grammars\JavaCC.javacc
 
 
--- bin/javacc.bat
+++ bin/javacc.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM If there is a generated javacc.jar in the base directory, use that, otherwise, use
-REM the bootstrap javacc.jar that is in the bin directory.
+REM If there are classfiles in the build directory, then use them, otherwise
+REM use the bootstrap javacc.jar that is in the bin directory.
 
 java -classpath "%~f0\..\..\build;%~f0\..\..\bootstrap\javacc.jar;%~f0\..\..\bootstrap\freemarker.jar" javacc.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
