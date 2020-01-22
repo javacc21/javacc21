a8429fa7ea975a71a655c9d798d5eb8fed6a7bf6
Jonathan Revusky <revusky@gmail.com>
2020-01-22 18:33:44 +0100

    Slight tweaks to the bin scripts

--- bin/buildparser.bat
+++ bin/buildparser.bat
@@ -3,6 +3,6 @@
 REM If there is a generated javacc.jar in the base directory, use that, otherwise, use
 REM the bootstrap javacc.jar that is in the bootstrap directory.
 
-java -classpath "%~f0\..\..\javacc.jar;%~f0\..\..\bootstrap\javacc.jar;%~f0\..\..\bootstrap\freemarker.jar" javacc.Main %~f0\..\..\src\grammars\JavaCC.javacc
+java -classpath "%~f0\..\..\build;%~f0\..\..\bootstrap\javacc.jar;%~f0\..\..\bootstrap\freemarker.jar" javacc.Main %~f0\..\..\src\grammars\JavaCC.javacc
 
 
