c56c1ee919e82d6f5f466692dc25ad983414d415
Jonathan Revusky <revusky@gmail.com>
2020-01-24 21:02:58 +0100

    Simplification, getting rid of bootstrap directory, wasn't a great idea.

--- bin/buildparser.bat
+++ bin/buildparser.bat
@@ -3,6 +3,7 @@
 REM Build the JavaCC grammar
 REM the bootstrap javacc.jar that is in the bootstrap directory.
 
-java -jar ""%~f0\..\javacc.jar" %~f0\..\..\src\grammars\JavaCC.javacc
+cd "%f0\.."
 
+javacc.bat ..\src\grammars\JavaCC.javacc
 
