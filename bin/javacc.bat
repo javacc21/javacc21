d37c9ce1fea582e625ab90cfe080d3e0e25bd931
Jonathan Revusky <revusky@gmail.com>
2020-04-04 23:40:40 +0200

    Have to adjust the scripts in bin directory, also rebootstrapping so that the build jar uses com.javacc.Main

--- bin/javacc.bat
+++ bin/javacc.bat
@@ -3,4 +3,4 @@
 REM If there are classfiles in the build directory, then use them, otherwise
 REM use the bootstrap javacc.jar that is in the bin directory.
 
-java -classpath "%~f0\..\..\build;%~f0\..\javacc.jar;%~f0\..\freemarker.jar" javacc.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%~f0\..\..\build;%~f0\..\javacc.jar;%~f0\..\freemarker.jar" com.javacc.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
