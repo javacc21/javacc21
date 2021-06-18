eee125c21439bc44fd63de78b9d091d42fbef76b
Jonathan Revusky <revusky@gmail.com>
2021-06-18 16:36:40 +0200

    Forgot to check this in

--- bin/buildparser.bat
+++ bin/buildparser.bat
@@ -3,5 +3,5 @@
 REM Build the JavaCC grammar
 REM the bootstrap javacc.jar that is in the bootstrap directory.
 
-java -jar %~f0\..\javacc.jar %~f0\..\..\src\main\grammars\JavaCC.javacc
+java -jar %~f0\..\javacc.jar %~f0\..\..\src\javacc\JavaCC.javacc
 
