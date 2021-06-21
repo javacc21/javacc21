08f9473847d15919fab8c0c486c38a2c2aad5995
Jonathan Revusky <revusky@Naomis-MacBook-Air.local>
2021-06-21 20:14:21 +0200

    Forgot to update these scripts. I don't know honestly whether anybody ever uses them.

--- bin/buildparser.bat
+++ bin/buildparser.bat
@@ -3,5 +3,6 @@
 REM Build the JavaCC grammar
 REM the bootstrap javacc.jar that is in the bootstrap directory.
 
+java -jar %~f0\..\javacc.jar -d %~f0\..\..\src\java %~f0\..\..\examples\preprocessor\Preprocessor.javacc
 java -jar %~f0\..\javacc.jar %~f0\..\..\src\javacc\JavaCC.javacc
 
