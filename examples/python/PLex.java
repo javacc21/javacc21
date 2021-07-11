2a661910c180397ec71d5bb1215caba856b41e4a
Jonathan Revusky <revusky@gmail.com>
2021-07-11 09:50:37 +0200

    Adding a test program solely for lexing python code

---
+++ examples/python/PLex.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
+import org.parsers.python.*;
+import org.parsers.python.PythonConstants.TokenType;
+
+/**
+ * A test harness for lexing Java files from 
+ * the command line.
+ */
+public class PLex {
+    
+    static public ArrayList<Node> roots= new ArrayList<>();
+
+   static public void main(String args[]) {
+      List<File> failures = new ArrayList<File>();
+      List<File> successes = new ArrayList<File>();    
+      if (args.length == 0) {
+        usage();
+      }
+      List<File> files = new ArrayList<File>();
+      for (String arg : args) {
+          File file = new File(arg);
+          if (!file.exists()) {
+              System.err.println("File " + file + " does not exist.");
+              continue;
+          }
+	   addFilesRecursively(files, file);
+      }
+      long startTime = System.currentTimeMillis();
+      int numTokens =0;
+      for (File file : files) {
+          try {
+              numTokens+=tokenizeFile(file);
+          } 
+          catch (Exception e) {
+              System.err.println("Error processing file: " + file);
+              e.printStackTrace();
+              failures.add(file);
+              continue;
+          }
+          System.out.println(file.getName()  + " tokenized successfully.");
+          successes.add(file);
+       }
+       if (!failures.isEmpty()) for (File file : failures) {
+           System.out.println("Lexing failed on: " + file);
+       }
+       System.out.println("\nTokenized " + successes.size() + " files, containing " + numTokens + " tokens.");
+       System.out.println("Failed on " + failures.size() + " files.");
+       System.out.println("\nDuration: " + (System.currentTimeMillis() - startTime) + " milliseconds");
+    }
+      
+   static public int tokenizeFile(File file) throws IOException, ParseException {
+       String content = new String(Files.readAllBytes(file.toPath()));
+       PythonLexer lexer = new PythonLexer(file.toString(), content);
+       Token t = null;
+       int numTokens = 0;
+       do {
+           t = lexer.getNextToken();
+           ++numTokens;
+       }
+       while (t.getType() != TokenType.EOF);
+       FileLineMap.clearFileLineMapCache();
+       return numTokens;
+   }
+
+   static public void addFilesRecursively(List<File> files, File file) {
+       if (file.isDirectory()) {
+           for (File f : file.listFiles()) {
+	         addFilesRecursively(files, f);
+	   }
+       }
+       else if (file.getName().endsWith(".py")) {
+           files.add(file);
+       }
+   }
+   
+   
+   static public void usage() {
+       System.out.println("Usage: java PLex <sourcefiles or directories>");
+       System.exit(-1);
+   }
+}
