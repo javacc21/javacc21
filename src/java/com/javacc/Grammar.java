0ff4d8a59f53ce3f0ddb4ebcbbd22d12cec9fa3c
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2021-10-26 17:16:46 +0100

    Partly address #106: Omit certain first sets from the parser which ap… (#127)
    * Partly address #106: Omit certain first sets from the parser which appear lexer-related.
    
    * Exclude some follow sets, too.

--- src/java/com/javacc/Grammar.java
+++ src/java/com/javacc/Grammar.java
@@ -583,6 +583,36 @@ public class Grammar extends BaseNode {
         List<Expansion> result = new ArrayList<>();
         for (Expansion expansion : descendants(Expansion.class)) {
             if (expansion.getParent() instanceof BNFProduction) continue; // Handle these separately
+            // Skip any sets which are related to the lexer
+            if (type == 0) {    // first sets
+                if ((expansion instanceof RegexpStringLiteral) ||
+                        (expansion instanceof ZeroOrMoreRegexp) ||
+                        (expansion instanceof ZeroOrOneRegexp) ||
+                        (expansion instanceof OneOrMoreRegexp) ||
+                        (expansion instanceof RegexpChoice) ||
+                        (expansion instanceof RegexpSequence) ||
+                        (expansion instanceof RegexpRef) ||
+                        (expansion instanceof CodeBlock) ||
+                        (expansion instanceof CharacterList)) {
+                    continue;
+                }
+            }
+//            else if (type == 1) {   // final sets
+//
+//            }
+            else if (type == 2) {   // follow sets
+                if ((expansion instanceof ZeroOrMoreRegexp) ||
+                        (expansion instanceof ZeroOrOneRegexp) ||
+                        (expansion instanceof OneOrMoreRegexp) ||
+                        (expansion instanceof RegexpChoice) ||
+                        (expansion instanceof RegexpSequence) ||
+                        // Allow RegexpRef as they are referring to tokens, which will often happen in the parser
+                        // (expansion instanceof RegexpRef) ||
+                        (expansion instanceof CodeBlock) ||
+                        (expansion instanceof CharacterList)) {
+                    continue;
+                }
+            }
             String varName = null;
             if (type==0) {
                 varName = expansion.getFirstSetVarName();
