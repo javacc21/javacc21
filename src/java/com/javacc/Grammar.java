792d6ed8d796122f4e3c6b2b95672f090d02120f
Jonathan Revusky <revusky@gmail.com>
2022-12-01 16:31:06 +0100

    Fixing the CSharp grammar so that it can work with LEGACY_GLITCHY_LOOKAHEAD turned off. This only required a change in one spot actually.

--- src/java/com/javacc/Grammar.java
+++ src/java/com/javacc/Grammar.java
@@ -1054,6 +1054,7 @@ public class Grammar extends BaseNode {
     public boolean getLegacyGlitchyLookahead() {
         Boolean b = (Boolean) settings.get("LEGACY_GLITCHY_LOOKAHEAD");
         return b == null || b;
+//        return b!=null && b;
     }
 
     public boolean getTreeBuildingEnabled() {
